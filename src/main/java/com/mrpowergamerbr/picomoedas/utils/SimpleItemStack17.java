89df543cef9635be26a09764b0e5042bd8710313
MrPowerGamerBR <leonardomalaman@gmail.com>
2016-11-06 07:53:38 -0200

    Adicionar SimpleItemStack17
    Ainda não é utilizado no código exceto no SimpleItemStack, já que o
    SimpleItemStack extende o SimpleItemStack17

---
+++ src/main/java/com/mrpowergamerbr/picomoedas/utils/SimpleItemStack17.java
@@ -0,0 +1,46 @@
+package com.mrpowergamerbr.picomoedas.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SimpleItemStack17 {
+    String name;
+    byte meta;
+    int quantidade = 1;
+    Material material;
+    List<String> lore = new ArrayList<String>();
+    HashMap<String, Integer> enchants = new HashMap<>();
+    
+    public ItemStack toItemStack() {
+        ItemStack item = new ItemStack(material, quantidade, meta);
+
+        ItemMeta meta = item.getItemMeta();
+
+        if (name != null) {
+            meta.setDisplayName(name);
+        }
+        
+        meta.setLore(lore);
+
+        item.setItemMeta(meta);
+        for (Entry<String, Integer> entry : enchants.entrySet()) {
+            item.addUnsafeEnchantment(Enchantment.getByName(entry.getKey()), entry.getValue());
+        }
+        
+        return item;
+    }
+}
