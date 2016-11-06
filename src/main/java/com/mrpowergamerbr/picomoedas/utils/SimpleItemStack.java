65a69b33816854afbab31d8a24533391a9f71c88
MrPowerGamerBR <leonardomalaman@gmail.com>
2016-11-06 09:10:22 -0200

    Remover variáveis duplicadas no SimpleItemStack

--- src/main/java/com/mrpowergamerbr/picomoedas/utils/SimpleItemStack.java
+++ src/main/java/com/mrpowergamerbr/picomoedas/utils/SimpleItemStack.java
@@ -1,11 +1,9 @@
 package com.mrpowergamerbr.picomoedas.utils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 
-import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
@@ -19,13 +17,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class SimpleItemStack extends SimpleItemStack17 {
-    String name;
-    byte meta;
-    int quantidade = 1;
-    Material material;
-    List<String> lore = new ArrayList<String>();
     List<ItemFlag> flags = new ArrayList<ItemFlag>();
-    HashMap<String, Integer> enchants = new HashMap<>();
     
     @Override
     public ItemStack toItemStack() {
