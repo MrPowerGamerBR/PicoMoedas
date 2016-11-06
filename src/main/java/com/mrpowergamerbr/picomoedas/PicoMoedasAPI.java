36ae04d91720b4349e0f282ea899fc23d2ed9957
MrPowerGamerBR <leonardomalaman@gmail.com>
2016-11-06 10:30:38 -0200

    Remover import desnecessário no PicoMoedasAPI

--- src/main/java/com/mrpowergamerbr/picomoedas/PicoMoedasAPI.java
+++ src/main/java/com/mrpowergamerbr/picomoedas/PicoMoedasAPI.java
@@ -1,6 +1,5 @@
 package com.mrpowergamerbr.picomoedas;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import com.mrpowergamerbr.picomoedas.ConfigValues.Type;
