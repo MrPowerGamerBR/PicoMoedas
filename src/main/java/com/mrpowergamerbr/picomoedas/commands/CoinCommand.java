e74f184d940b5764346481be50870f78eaee71a7
MrPowerGamerBR <leonardomalaman@gmail.com>
2016-11-06 10:38:40 -0200

    Adicionar "/coins nomedoplayer"
    TODO: Falta adicionar suporte ao MeninaAPI

--- src/main/java/com/mrpowergamerbr/picomoedas/commands/CoinCommand.java
+++ src/main/java/com/mrpowergamerbr/picomoedas/commands/CoinCommand.java
@@ -10,6 +10,8 @@ import com.mrpowergamerbr.picomoedas.ConfigValues;
 import com.mrpowergamerbr.picomoedas.PicoMoedasAPI;
 import com.mrpowergamerbr.picomoedas.ConfigValues.Type;
 import com.mrpowergamerbr.picomoedas.utils.AbstractCommand;
+import com.mrpowergamerbr.picomoedas.utils.CommandUtils;
+import com.mrpowergamerbr.picomoedas.utils.MeninaWrapper;
 
 public class CoinCommand extends AbstractCommand {
 
@@ -20,13 +22,24 @@ public class CoinCommand extends AbstractCommand {
     public CoinCommand(String command, String usage, String description, List<String> aliases, String comoUsa) {
         super(command, usage, description, aliases);
     }
-    
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        Player p = (Player) sender;
-        String moedas = ConfigValues.getFromConfigColorized(Type.SUAS_MOEDAS);
-        moedas = moedas.replace("{@moedas}", String.valueOf(PicoMoedasAPI.getBalance(p).getValue()));
-        sender.sendMessage(moedas);
+        if (CommandUtils.argExists(args, 1)) {
+            String moedas = ConfigValues.getFromConfigColorized(Type.SUAS_MOEDAS);
+            moedas = moedas.replace("{@moedas}", String.valueOf(PicoMoedasAPI.getBalance(args[0]).getValue()));
+            moedas = moedas.replace("{@artigo}", MeninaWrapper.getCorrectArtigo(args[0]));
+            sender.sendMessage(moedas);
+            return true;
+        } else {
+            if (CommandUtils.isPlayer(sender)) {
+                Player p = (Player) sender;
+                String moedas = ConfigValues.getFromConfigColorized(Type.SUAS_MOEDAS);
+                moedas = moedas.replace("{@moedas}", String.valueOf(PicoMoedasAPI.getBalance(p).getValue()));
+                sender.sendMessage(moedas);
+                return true;
+            }
+        }
         return true;
     }
 }
--- src/main/java/com/mrpowergamerbr/picomoedas/utils/CommandUtils.java
+++ src/main/java/com/mrpowergamerbr/picomoedas/utils/CommandUtils.java
@@ -1,5 +1,8 @@
 package com.mrpowergamerbr.picomoedas.utils;
 
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+
 public class CommandUtils {
     public static boolean arg(String[] arg, int i, String s) {
         try {
@@ -25,5 +28,13 @@ public class CommandUtils {
         catch (Exception e) {
             return false;
         }
+    }
+    
+    public static boolean isPlayer(CommandSender sender) {
+        return !isConsole(sender);
+    }
+    
+    public static boolean isConsole(CommandSender sender) {
+        return sender instanceof ConsoleCommandSender;
     }
 }
